9d3beefc3145fd694c5f94ce9e5150702ce0f4ce
abdulrahman20231 <133828060+abdulrahman20231@users.noreply.github.com>
2024-01-29 23:54:22 -0600

    Update CO2_Solst.py

--- CO2_Solst.py
+++ CO2_Solst.py
@@ -3,9 +3,12 @@ import streamlit as st
 import pickle
 import base64
 import numpy as np
-from PIL import Image
 
-# Function to download data as csv
+import requests
+# df = pd.read_csv("C:/Utilities/MMP Papers/HC inj code/MMP_HC_Data ALLwith SG_Clean.csv")
+# read a CSV file inside the 'data" folder next to 'app.py'
+# df = pd.read_excel(...)  # will work for Excel files
+ #Function to download data as csv
 def download_link(object_to_download, download_filename):
     if isinstance(object_to_download, pd.DataFrame):
         object_to_download = object_to_download.to_csv(index=False)
@@ -14,6 +17,10 @@ def download_link(object_to_download, download_filename):
     href = f'data:file/csv;base64,{b64}'
     return f'<a href="{href}" download="{download_filename}">Download Results</a>'
 
+# Function to predict solubility
+# Function to predict solubility
+# Function to predict solubility
+# Function to predict solubility
 # Function to predict solubility
 def predict_solubility(data0):
     P = data0['P,Psia']
@@ -77,43 +84,51 @@ def predict_solubility(data0):
     return results
 
 html_temp = """
-<div class="header">
-    <h1 style="color:white;">UH CO2 Solubility in Brine Calculator</h1>
+<div style="background-color:tomato;padding:1.5px">
+<h1 style="color:white;text-align:center;">UH CO2 Solubility in Brine Calculator  </h1>
 </div><br>"""
 st.markdown(html_temp, unsafe_allow_html=True)
 
-st.header("Department of Petroleum Engineering: Interaction of Phase Behavior and Flow in Porous Media (IPBFPM) Consortium.")
-st.subheader("Product Description - Calculates the CO2 Solubility in Brine for Different 9 Salts type at specific Pressure and Temperature.")
-st.subheader("[Download Input Template File.](https://drive.google.com/file/d/1IrmFmwePqceAU4qlLBqsmssMtXlAe1Pm/view?usp=sharing)")
+st.header(
+    "Department of Petroleum Engineering: Interaction of Phase Behavior and Flow in Porous Media ([IPBFPM](https://dindoruk.egr.uh.edu/)) Consortium.")
 
-# Upload file
+st.markdown('<style>h2{color: red;}</style>', unsafe_allow_html=True)
+st.subheader(
+    "Product Description - Calculates the CO2 Solubility in Brine for Different 9 Salts type at specific Pressure and Temperature.")
+st.markdown('<style>h2{color: red;}</style>', unsafe_allow_html=True)
+st.subheader(
+    "[Download Input Template File.](https://drive.google.com/file/d/1IrmFmwePqceAU4qlLBqsmssMtXlAe1Pm/view?usp=sharing)")
+# st.markdown("[Input Template File Link](https://drive.google.com/file/d/1HNyZjobmTEBcWfk0C2cmClQfahTONrX1/view?usp=sharing)",unsafe_allow_html=True)
+
+
 file = st.file_uploader("Upload the CSV file", type=['csv'])
 
 if file is not None:
     # Load the data
     data = pd.read_csv(file)
-
     # Display the loaded data
     st.subheader('Loaded Data:')
     st.write(data)
-
     # Call the predict function
     results = predict_solubility(data)
 
     # Display the result table
-    if st.button('Predict', key='predict_button'):
+    if st.button('Predict'):
         st.write(results)
 
     # Download the results as csv
-    if st.button('Download Results', key='download_button'):
+    if st.button('Download Results'):
         csv_data = results.to_csv(index=False)
         tmp_download_link = download_link(csv_data, 'CO2_Solubility_Results.csv')
         st.markdown(tmp_download_link, unsafe_allow_html=True)
 
-# Reference
-st.subheader("Reference:")
-st.markdown("[Based on the work in Ref;Ratnakar, R. R., Chaubey, V., & Dindoruk, B. (2023). A novel computational strategy to estimate CO2 solubility in brine solutions for CCUS applications. Applied Energy, 342, 121134.](https://www.sciencedirect.com/science/article/pii/S0306261923004981?casa_token=kPpCANAGDIUAAAAA:IGNAx8egWSeRs54UtPnUG1C9OLRKir1DOGPwYm7O2nfeWCP4wKqsCY46_sJGVrk9-YgDrclfGzB4)")
+st.subheader(
+    "[Based on the work in Ref;Ratnakar, R. R., Chaubey, V., & Dindoruk, B. (2023). A novel computational strategy to estimate CO2 solubility in brine solutions for CCUS applications. Applied Energy, 342, 121134.](https://www.sciencedirect.com/science/article/pii/S0306261923004981?casa_token=kPpCANAGDIUAAAAA:IGNAx8egWSeRs54UtPnUG1C9OLRKir1DOGPwYm7O2nfeWCP4wKqsCY46_sJGVrk9-YgDrclfGzB4)")
+# st.subheader('[Ref.:Ratnakar, R. R., Chaubey, V., & Dindoruk, B. (2023). A novel computational strategy to estimate CO2 solubility in brine solutions for CCUS applications. Applied Energy, 342, 121134.](https://www.sciencedirect.com/science/article/pii/S0306261923004981?casa_token=kPpCANAGDIUAAAAA:IGNAx8egWSeRs54UtPnUG1C9OLRKir1DOGPwYm7O2nfeWCP4wKqsCY46_sJGVrk9-YgDrclfGzB4)')
 
-# University logo
+from PIL import Image
+
 image = Image.open('uhlogo.jpg')
-st.image(image, caption='A product of University of Houston', use_column_width=True)
+st.image(image, caption='A product of University of Houston')
+
+# print('result===',result)
